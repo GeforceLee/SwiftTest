60755da8364dc2c55466aede2c2aa8619dae1315
geforce <liyunlong880601@gmail.com>
2016-02-27 17:19:35 +0800

    init

---
+++ SwiftTest/SwiftTest.h
@@ -0,0 +1,19 @@
+//
+//  SwiftTest.h
+//  SwiftTest
+//
+//  Created by geforce on 16/2/27.
+//  Copyright (c) 2016年 geforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftTest.
+FOUNDATION_EXPORT double SwiftTestVersionNumber;
+
+//! Project version string for SwiftTest.
+FOUNDATION_EXPORT const unsigned char SwiftTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftTest/PublicHeader.h>
+
+
